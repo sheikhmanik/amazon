ce7e9b89bbbfbc4b9f6295b921e68911f92b869f
Md. Manik Ullah <md.manikullah@Mds-MacBook-Air.local>
2025-04-17 02:39:21 +0530

    home updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,9 @@ import Products from "./home-page/products/Products";
 
 export default function Home() {
     return (
-        <div className="flex flex-col items-center justify-center gap-10 sm:gap-12">
+        <div className="flex flex-col items-center justify-center pb-10 gap-10 sm:gap-12">
             <Hero autoSlide={true} autoSlideInterval={5000} />
-            <div className="w-screen max-w-[1536px] flex flex-col gap-14 sm:gap-16 py-10 pb-20">
+            <div className="w-screen max-w-[1536px] flex flex-col gap-14 sm:gap-16 py-7">
                 <Products category="All" title="All Products" />
                 <Products category="Beauty" title="Beauty Products" />
                 <Products category="Fragrances" title="Fragrances Products" />
