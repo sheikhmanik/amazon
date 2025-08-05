cfd02e73ddc81aad1198b76ca7029e9d7a63f472
Md. Manik Ullah <md.manikullah@Mds-MacBook-Air.local>
2025-08-05 16:04:58 +0530

    port issue fixed

--- server/server.js
+++ server/server.js
@@ -2,14 +2,21 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import { productsRoutes } from './routes/products.js';
+
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ['https://amazon-dnzu.onrender.com'],
+    credentials: true
+}));
+app.use(express.json()); // To parse incoming JSON
 
 app.use('/api', productsRoutes);
 
-app.listen(3000, () => {
-    console.log("Server started at port 3000.")
-})
+// Use dynamic port for Render
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+});
